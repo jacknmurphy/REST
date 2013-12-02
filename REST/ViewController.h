3e301eea1080a59a954a875afc2a317bde770e0b
Jack Murphy <Jack@socs-MacBook-Pro.local>
2013-12-02 23:37:55 +0000

    Initial Commit

---
+++ REST/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  REST
+//
+//  Created by Jack Murphy on 02/12/2013.
+//  Copyright (c) 2013 Jack Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ REST/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  REST
+//
+//  Created by Jack Murphy on 02/12/2013.
+//  Copyright (c) 2013 Jack Murphy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
